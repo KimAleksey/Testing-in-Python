328750e2fc12dde384afc5633244d888076ff105
AlekseiKim <kimalex8589@gmail.com>
2025-12-21 21:48:46 +0300

    Added tags to tests

--- tests/test_typed_simple_function.py
+++ tests/test_typed_simple_function.py
@@ -2,25 +2,26 @@ import pytest
 
 from main.main import return_only_int
 
+class TestReturnOnlyInt:
+    @pytest.mark.unit
+    def test_typed_simple_function_int(self):
+        assert return_only_int(5) == 5
 
-def test_typed_simple_function_int():
-    assert return_only_int(5) == 5
+    @pytest.mark.unit
+    def test_typed_simple_function_none(self):
+        assert return_only_int(None) is None
 
+    @pytest.mark.unit
+    def test_typed_simple_function_str_raises(self):
+        with pytest.raises(TypeError):
+            return_only_int("hello")
 
-def test_typed_simple_function_none():
-    assert return_only_int(None) is None
+    @pytest.mark.unit
+    def test_typed_simpel_function_float_raises(self):
+        with pytest.raises(TypeError):
+            return_only_int(1.1)
 
-
-def test_typed_simple_function_str_raises():
-    with pytest.raises(TypeError):
-        return_only_int("hello")
-
-
-def test_typed_simpel_function_float_raises():
-    with pytest.raises(TypeError):
-        return_only_int(1.1)
-
-
-def test_typed_simpel_function_list_raises():
-    with pytest.raises(TypeError):
-        return_only_int([1, 2, 3])
+    @pytest.mark.unit
+    def test_typed_simpel_function_list_raises(self):
+        with pytest.raises(TypeError):
+            return_only_int([1, 2, 3])
