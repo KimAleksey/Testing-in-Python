df5352100805719b4fa168ee8c00907d2e769b1b
AlekseiKim <kimalex8589@gmail.com>
2025-12-21 20:39:12 +0300

    commit

--- tests/test_typed_simple_function.py
+++ tests/test_typed_simple_function.py
@@ -2,16 +2,25 @@ import pytest
 
 from main.main import return_only_int
 
+
 def test_typed_simple_function_int():
     assert return_only_int(5) == 5
+
 
 def test_typed_simple_function_none():
     assert return_only_int(None) is None
 
-def test_typed_simple_function_str():
+
+def test_typed_simple_function_str_raises():
     with pytest.raises(TypeError):
         return_only_int("hello")
 
-def test_typed_simpel_function_float():
+
+def test_typed_simpel_function_float_raises():
     with pytest.raises(TypeError):
         return_only_int(1.1)
+
+
+def test_typed_simpel_function_list_raises():
+    with pytest.raises(TypeError):
+        return_only_int([1, 2, 3])
